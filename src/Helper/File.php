ca2f5afb5f9da0fb3750a21a370644f90f09d810
yangsuda <919579@qq.com>
2020-12-14 10:05:36 +0800

    日志记录方法位置调整

--- src/Helper/File.php
+++ src/Helper/File.php
@@ -8,6 +8,12 @@
 
 namespace SlimCMS\Helper;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Monolog\Formatter\LineFormatter;
+use Psr\Log\LoggerInterface;
+
 class File
 {
 
@@ -33,16 +39,49 @@ class File
 
     /**
      * 日志记录
+     * @param string $dir
+     * @param string $logName
+     * @param string $format
+     * @return Logger
+     */
+    public static function log(string $dir = '', string $logName = 'slimCMS', string $format = 'Y-m-d'): Logger
+    {
+        static $loggers = [];
+        $key = $dir . $format . $logName;
+        if (empty($loggers[$key])) {
+            //日期格式
+            $dateFormat = "Y-m-d H:i:s";
+            //输出格式
+            $output = "[%datetime%] - [%level_name%] - [%channel%]\n%message% %context%\n";
+            //创建一个格式化器
+            $formatter = new LineFormatter($output, $dateFormat);
+
+            $dir = trim($dir, '/');
+            $config = getConfig();
+            $suffix = substr(md5($config['setting']['security']['authkey']), 5, -10);
+            $dir = ($dir ? '_' : '') . $suffix;
+
+            $path = CSDATA . $dir . '/' . date($format) . '.log';
+            $logger = new Logger($logName);
+            $handler = new StreamHandler($path, 100);
+            $handler->setFormatter($formatter);
+            $loggers[$key] = $logger->setHandlers([$handler]);
+        }
+        return $loggers[$key];
+    }
+
+    /**
+     * 日志记录
      * @param $msg
      * @param string $dir
      * @param string $format
      */
-    public static function log($msg, $dir = 'log',$format='Y-m-d')
+    public static function log1($msg, $dir = 'log', $format = 'Y-m-d')
     {
-        $path = CSDATA . '/' . $dir . '/' . date('Y') . '/';
+        $path = CSDATA . trim($dir, '/') . '/';
         self::mkdir($path);
-        $logurl = $path . date($format,TIMESTAMP) . '.php';
-        !is_file($logurl) && @file_put_contents ( $logurl, "<?php\n exit();\n?>\n", FILE_APPEND );
-        file_put_contents ( $logurl,  date('Y-m-d H:i:s') . ":\n" . $msg . "\n", FILE_APPEND );
+        $logurl = $path . date($format, TIMESTAMP) . '.php';
+        !is_file($logurl) && @file_put_contents($logurl, "<?php\n exit();\n?>\n", FILE_APPEND);
+        file_put_contents($logurl, date('Y-m-d H:i:s') . ":\n" . $msg . "\n", FILE_APPEND);
     }
 }

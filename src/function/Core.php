24f888fb16d6f94e860dc8333a476d85932ba565
yangsuda <919579@qq.com>
2021-04-09 08:56:59 +0800

    修复debug关闭，生成缓存后，导致clienttype、referer值也被缓存问题
    生成CompiledContainer.php缓存，导致clienttype、referer值不能实时变化

--- src/function/Core.php
+++ src/function/Core.php
@@ -36,24 +36,30 @@ function getConfig()
             $settings = require_once CSROOT . 'config/settings.php';
             $cfg = array_merge($cfg, $settings);
         }
-        $cfg['cfg']['referer'] = aval($_SERVER, 'HTTP_REFERER');
-        $cfg['cfg']['clienttype'] = 0;
 
         //防止最后不加/导致ueditor等加载出错
         $cfg['cfg']['basehost'] = rtrim($cfg['cfg']['basehost'], '/') . '/';
 
-        $agent = $_SERVER['HTTP_USER_AGENT'];
-        if (!empty($cfg['referer']) && strpos($cfg['referer'], 'servicewechat.com')) {
-            $cfg['cfg']['clienttype'] = 2;//微信小程序
-        } elseif (preg_match('/MicroMessenger/i', $agent)) {
-            $cfg['cfg']['clienttype'] = 3;//微信WAP
-        } elseif (preg_match('/NetFront|iPhone|MIDP-2.0|Opera Mini|UCWEB|Android|Windows CE/i', $agent)) {
-            $cfg['cfg']['clienttype'] = 1;//WAP
-        }
         if (strpos(aval($_SERVER, 'HTTP_ACCEPT_ENCODING'), 'gzip') === false
             || !function_exists('ob_gzhandler')) {
             $cfg['settings']['output']['gzip'] = false;
         }
+        $cfg['cfg']['clienttype'] = function (){
+            $agent = $_SERVER['HTTP_USER_AGENT'];
+            $referer = aval($_SERVER, 'HTTP_REFERER');
+            $clienttype = 0;
+            if (!empty($referer) && strpos($referer, 'servicewechat.com')) {
+                $clienttype = 2;//微信小程序
+            } elseif (preg_match('/MicroMessenger/i', $agent)) {
+                $clienttype = 3;//微信WAP
+            } elseif (preg_match('/NetFront|iPhone|MIDP-2.0|Opera Mini|UCWEB|Android|Windows CE/i', $agent)) {
+                $clienttype = 1;//WAP
+            }
+            return $clienttype;
+        };
+        $cfg['cfg']['referer'] = function (){
+            return aval($_SERVER, 'HTTP_REFERER');
+        };
     }
     return $cfg;
 }

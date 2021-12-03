a30b651f8bbd5d113259a01f859c8ddbddaa6cb9
yangsuda <919579@qq.com>
2021-12-03 15:07:46 +0800

    修复debug关闭后日志记录不到具体错误信息问题

--- src/Handlers/ShutdownHandler.php
+++ src/Handlers/ShutdownHandler.php
@@ -26,15 +26,16 @@ class ShutdownHandler
     /**
      * ShutdownHandler constructor.
      *
-     * @param Request       $request
+     * @param Request $request
      * @param $errorHandler $errorHandler
-     * @param bool          $displayErrorDetails
+     * @param bool $displayErrorDetails
      */
     public function __construct(
         Request $request,
         HttpErrorHandler $errorHandler,
         bool $displayErrorDetails
-    ) {
+    )
+    {
         $this->request = $request;
         $this->errorHandler = $errorHandler;
         $this->displayErrorDetails = $displayErrorDetails;
@@ -48,28 +49,24 @@ class ShutdownHandler
             $errorLine = $error['line'];
             $errorMessage = $error['message'];
             $errorType = $error['type'];
-            $message = 'An error while processing your request. Please try again later.';
+            switch ($errorType) {
+                case E_USER_ERROR:
+                    $message = "FATAL ERROR: {$errorMessage}. ";
+                    $message .= " on line {$errorLine} in file {$errorFile}.";
+                    break;
 
-            if ($this->displayErrorDetails) {
-                switch ($errorType) {
-                    case E_USER_ERROR:
-                        $message = "FATAL ERROR: {$errorMessage}. ";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
+                case E_USER_WARNING:
+                    $message = "WARNING: {$errorMessage}";
+                    break;
 
-                    case E_USER_WARNING:
-                        $message = "WARNING: {$errorMessage}";
-                        break;
+                case E_USER_NOTICE:
+                    $message = "NOTICE: {$errorMessage}";
+                    break;
 
-                    case E_USER_NOTICE:
-                        $message = "NOTICE: {$errorMessage}";
-                        break;
-
-                    default:
-                        $message = "ERROR: {$errorMessage}";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
-                }
+                default:
+                    $message = "ERROR: {$errorMessage}";
+                    $message .= " on line {$errorLine} in file {$errorFile}.";
+                    break;
             }
 
             $exception = new HttpInternalServerErrorException($this->request, $message);
